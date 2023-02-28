21334d0e6160b7b0e3ed8e5942cd90da495d320f
Võ Văn Thành <63183734+thanhdau@users.noreply.github.com>
2023-03-01 02:02:28 +0700

    n01032023

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,3 @@
-const slides = document.getElementsByClassName("slide");
-let slideIndex = 0;
-slideShow(slideIndex);
-
-// prev/next slide
-function plusSlides(n) {
-  slideShow((slideIndex += n));
-}
-// Show slides with
-function slideShow(n) {
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
-
-function showSlidesAuto() {
-  let i;
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlidesAuto, 5000);
-}
-showSlidesAuto();
 //slide-slick
 $(document).ready(function () {
   $(".activity__list").slick({
@@ -72,4 +36,76 @@ AOS.init({
   duration: 1000,
 });
 
-//Get date even calendar
+/*Active navbar click */
+// tablet and moblie
+$(document).ready(function () {
+  $(".navbar-list li a").click(function () {
+    $(".navbar-list li a.active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+// menu toggle
+$('.navbar-btn--open').click(function(event) {
+  $(".navbar-btn").toggleClass("active");
+  // $(".navbar-btn").toggleClass('active', $(this).hasClass('active'));
+});
+$('.navbar-btn--close').click(function(event) {
+  $(".navbar-btn.active").removeClass("active");
+});
+//nav child
+$('.nav-lv-3').click(function(event) {
+  $(this).toggleClass("open");
+  $('.nav-lv-3').toggleClass('open', $(this).hasClass('open'));
+});
+//mb evencalendar
+$('.current-week__table-mb').click(function(event) {
+  $(this).toggleClass("open");
+  $(this).toggleClass('open', $(this).hasClass('open'));
+});
+// const x = document.querySelectorAll('.current-week__table-mb');
+// x.forEach(function(item) {
+//   item.addEventListener('click', function(){
+//     x.forEach(btn =>{btn.classList.remove('open')})
+//     this.classList.add('open');
+//   })
+// })
+
+
+
+//slide index
+const slides = document.getElementsByClassName("slide");
+let slideIndex = 0;
+slideShow(slideIndex);
+
+// prev/next slide
+function plusSlides(n) {
+  slideShow((slideIndex += n));
+}
+// Show slides with
+function slideShow(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+function showSlidesAuto() {
+  let i;
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlidesAuto, 5000);
+}
+showSlidesAuto();
